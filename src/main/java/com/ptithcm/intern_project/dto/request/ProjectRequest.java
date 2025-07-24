22d8572864ab67bb06a572d4e564711f0685ae1a
levandung-it03 <levandung.it03@gmail.com>
2025-07-24 19:48:34 +0700

    chore backup

---
+++ src/main/java/com/ptithcm/intern_project/dto/request/ProjectRequest.java
@@ -0,0 +1,13 @@
+package com.ptithcm.intern_project.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProjectRequest {
+}
---
+++ src/main/java/com/ptithcm/intern_project/service/ProjectService.java
@@ -0,0 +1,4 @@
+package com.ptithcm.intern_project.service;
+
+public class ProjectService {
+}
