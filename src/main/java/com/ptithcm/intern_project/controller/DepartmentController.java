1f37784b23039c9736c33785b638cd0566641564
levandung-it03 <levandung.it03@gmail.com>
2025-12-12 19:41:32 +0700

    fix: implemented

--- src/main/java/com/ptithcm/intern_project/controller/DepartmentController.java
+++ src/main/java/com/ptithcm/intern_project/controller/DepartmentController.java
@@ -28,13 +28,13 @@ public class DepartmentController {
 
     @Operation(description = "Create Department")
     @ApiResponses(value = {})
-    @PostMapping(ROLE_EMP + "/v1/department")
+    @PostMapping(ROLE_ADMIN + "/v1/department")
     public ResponseEntity<RestApiResponse<IdResponse>> create(@Valid @RequestBody DepartmentRequest request) {
         return RestApiResponse.fromScs(SuccessCodes.CREATED, departmentService.create(request));
     }
 
     @Operation(description = "Update specified Department")
-    @PutMapping(ROLE_EMP + "/v1/department/{id}")
+    @PutMapping(ROLE_ADMIN + "/v1/department/{id}")
     public ResponseEntity<RestApiResponse<Void>> update(@PathVariable Long id,
                                                         @Valid @RequestBody DepartmentRequest request) {
         departmentService.update(id, request);
@@ -42,20 +42,28 @@ public class DepartmentController {
     }
 
     @Operation(description = "Delete specified Department")
-    @DeleteMapping(ROLE_EMP + "/v1/department/{id}")
+    @DeleteMapping(ROLE_ADMIN + "/v1/department/{id}")
     public ResponseEntity<RestApiResponse<Void>> delete(@PathVariable Long id) {
         departmentService.delete(id);
         return RestApiResponse.fromScs(SuccessCodes.DELETED);
     }
 
     @Operation(description = "Get specified Department")
-    @GetMapping({ROLE_EMP + "/v1/department/{id}", ROLE_LEAD + "/v1/department/{id}", ROLE_EMP + "/v1/department/{id}"})
+    @GetMapping({
+        ROLE_ADMIN + "/v1/department/{id}",
+        ROLE_EMP + "/v1/department/{id}",
+        ROLE_LEAD + "/v1/department/{id}",
+        ROLE_EMP + "/v1/department/{id}"})
     public ResponseEntity<RestApiResponse<Department>> get(@PathVariable Long id) {
         return RestApiResponse.fromScs(SuccessCodes.GET_DETAIL, departmentService.get(id));
     }
 
     @Operation(description = "Get all Departments")
-    @GetMapping({ROLE_EMP + "/v1/department/all", ROLE_LEAD + "/v1/department/all", ROLE_EMP + "/v1/department/all"})
+    @GetMapping({
+        ROLE_ADMIN + "/v1/department/all",
+        ROLE_EMP + "/v1/department/all",
+        ROLE_LEAD + "/v1/department/all",
+        ROLE_EMP + "/v1/department/all"})
     public ResponseEntity<RestApiResponse<List<Department>>> getAll() {
         return RestApiResponse.fromScs(SuccessCodes.GET_LIST, departmentService.getAll());
     }
